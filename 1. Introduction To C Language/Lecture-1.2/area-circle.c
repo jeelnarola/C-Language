5ceaaec3bdaa012e21432eb9c8d18c9fd7773cc9
jeel narola <narolajeel29@gmail.com>
2023-12-11 16:52:01 +0530

    c

---
+++ 1. Introduction To C Language/Lecture-1.2/area-circle.c
@@ -0,0 +1,17 @@
+
+// 1 Write a Program to find the area of a circle.
+
+#include<stdio.h>
+#define pi 3.14
+int main()
+{
+    int a,area;
+    printf("enater a A number =>",a);
+    scanf("%d",&a);
+
+    area=pi*a*a;  // =pi*r*r => 3.14*a*a
+    
+    printf("area of a circle ANS => %d",area);
+
+
+}
